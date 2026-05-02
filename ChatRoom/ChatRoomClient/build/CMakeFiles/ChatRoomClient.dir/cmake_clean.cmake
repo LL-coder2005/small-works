efbb58d2b805f04b7010c04fb26f9ad28adc0e0c
LL-coder2005 <674078897@qq.com>
2026-05-02 04:13:56 -0400

    使用cmake管理项目文件

---
+++ ChatRoom/ChatRoomClient/build/CMakeFiles/ChatRoomClient.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatRoomClient.dir/src/ChatRoomClient.cpp.o"
+  "CMakeFiles/ChatRoomClient.dir/src/ChatRoomClient.cpp.o.d"
+  "CMakeFiles/ChatRoomClient.dir/src/main.cpp.o"
+  "CMakeFiles/ChatRoomClient.dir/src/main.cpp.o.d"
+  "ChatRoomClient"
+  "ChatRoomClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatRoomClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ChatRoom/ChatRoomServer/build/CMakeFiles/ChatRoomServer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatRoomServer.dir/src/ChatRoomSer.cpp.o"
+  "CMakeFiles/ChatRoomServer.dir/src/ChatRoomSer.cpp.o.d"
+  "CMakeFiles/ChatRoomServer.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/ChatRoomServer.dir/src/ThreadPool.cpp.o.d"
+  "CMakeFiles/ChatRoomServer.dir/src/main.cpp.o"
+  "CMakeFiles/ChatRoomServer.dir/src/main.cpp.o.d"
+  "ChatRoomServer"
+  "ChatRoomServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatRoomServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
